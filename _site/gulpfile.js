ca2bd0317cbccc049bb5e5d86f3babbb94add0ac
James Cool <jeffreysbrother@gmail.com>
2016-06-08 13:56:16 -0700

    re-build and deploy

--- _site/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-
-gulp.task('imagemin', function() {
-	gulp.src('img-src/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('img'))
-});
