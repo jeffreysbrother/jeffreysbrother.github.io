5457c103a35aa77f71d8172a98c45101911a89b6
James Cool <jeffreysbrother@gmail.com>
2021-02-22 19:17:04 -0800

    remove Jekyll blog stuff

--- scripts/nav.js
+++
@@ -1,8 +0,0 @@
-$("#toggle").on('click', function (e) {
-	e.preventDefault();
-	$('body, .mobile-navbar').toggleClass('js-open');
-});
-
-$(".nav-background").on('click', function () {
-  $('body, .mobile-navbar').removeClass('js-open');
-});
