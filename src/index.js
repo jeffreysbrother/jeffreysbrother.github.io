8f11922a4135641d9c7fd067cd345290de36a6cb
james cool <jamescool@jamess-MacBook-Pro.local>
2023-03-20 19:02:26 -0700

    whoops

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import Homepage from './pages/Homepage';
 import Acoustics from './pages/Acoustics';
 import NotFound from './pages/NotFound';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('main-container'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="acoustics" element={<Acoustics />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
