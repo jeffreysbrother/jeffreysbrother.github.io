b9db3bf83c7f439742c0e1ea566364118b65aea6
james cool <jamescool@jamess-MacBook-Pro.local>
2023-03-20 17:45:41 -0700

    update browser router code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Homepage from './Homepage';
+import Homepage from './pages/Homepage';
 import Acoustics from './pages/Acoustics';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/acoustics",
-    element: <Acoustics />,
-  },
-]);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('main-container'));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="acoustics" element={<Acoustics />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,5 @@
 import './Homepage.css';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Homepage = () => (
   <div id="homepage">
