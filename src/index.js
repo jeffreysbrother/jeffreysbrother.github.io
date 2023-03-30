5acd975fae6b59df4f73d61ea24bbfd8e4c93ad0
james cool <jamescool@jamess-MacBook-Pro.local>
2023-03-29 20:33:26 -0700

    use hash router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import Homepage from './pages/Homepage';
 import Acoustics from './pages/Acoustics';
 import NotFound from './pages/NotFound';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('main-container'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="acoustics" element={<Acoustics />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
